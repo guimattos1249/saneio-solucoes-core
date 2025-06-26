b8437c268f077483369c50afd4871a8671ef4e85
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-26 13:05:11 -0300

    feat: adding extension to get Connection string and to know if is test enviroment

---
+++ src/Backend/SaneioSolucoes.Infrastructure/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SaneioSolucoes.Infrastructure.Extensions
+{
+    public static class ConfigurationExtension
+    {
+        public static bool IsUnitTestEnviroment(this IConfiguration configuration) => configuration.GetValue<bool>("InMemoryTest");
+
+        public static string ConnectionString(this IConfiguration configuration) => configuration.GetConnectionString("Connection")!;
+    }
+}
