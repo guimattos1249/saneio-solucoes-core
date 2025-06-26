e8a53d5c07b1fde6586b8ebf3541be8e3e9281da
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-26 13:09:39 -0300

    chore: configured appsettings and Program.cs

--- src/Backend/SaneioSolucoes.API/Program.cs
+++ src/Backend/SaneioSolucoes.API/Program.cs
@@ -1,17 +1,66 @@
+using Microsoft.OpenApi.Models;
+using MyRecipeBook.API.Token;
+using SaneioSolucoes.API.Converters;
+using SaneioSolucoes.Domain.Security.Tokens;
+using SaneioSolucoes.Infrastructure;
+using SaneioSolucoes.Infrastructure.Extensions;
+using SaneioSolucoes.Infrastructure.Migrations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new StringConverter()));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Jwt Authorization header using the Bearer scheme.
+                        Enter 'Bearer' [space] and then your token in the text input below.
+                        Exemple? 'Bearer 12345abcde'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+//builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+//builder.Services.AddApplication(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddScoped<ITokenProvider, HttpContextTokenValue>();
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -20,4 +69,23 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+MigrateDatabase();
+
 app.Run();
+
+void MigrateDatabase()
+{
+if (builder.Configuration.IsUnitTestEnviroment())
+return;
+
+var connectionString = builder.Configuration.ConnectionString();
+
+var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+
+DatabaseMigration.Migrate(connectionString, serviceScope.ServiceProvider);
+}
+
+public partial class Program
+{
+    protected Program() { }
+}
