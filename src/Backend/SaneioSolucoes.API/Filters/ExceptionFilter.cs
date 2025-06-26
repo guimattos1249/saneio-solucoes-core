273ac88356da85349c95b8a9617f6d33ed8956ed
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-26 18:39:28 -0300

    feat: adding exception filter

---
+++ src/Backend/SaneioSolucoes.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using SaneioSolucoes.Communication.Responses;
+using SaneioSolucoes.Exceptions.ExceptionBase;
+using SaneioSolucoes.Exceptions;
+using System.Net;
+
+namespace SaneioSolucoes.API.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is SaneioSolucoesException)
+                HandleProjectException(context);
+            else
+                ThrowUnknowException(context);
+        }
+
+        private static void HandleProjectException(ExceptionContext context)
+        {
+            if (context.Exception is ErrorOnValidationException)
+            {
+                var exception = context.Exception as ErrorOnValidationException;
+
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Result = new BadRequestObjectResult(new ResponseErrorJson(exception!.ErrorMessages));
+            }
+            else if (context.Exception is InvalidLoginException)
+            {
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                context.Result = new UnauthorizedObjectResult(new ResponseErrorJson(context.Exception.Message));
+            }
+            else if (context.Exception is NotFoundException)
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                context.Result = new ObjectResult(new ResponseErrorJson(context.Exception.Message));
+            }
+        }
+
+        private static void ThrowUnknowException(ExceptionContext context)
+        {
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Result = new ObjectResult(new ResponseErrorJson(ResourceMessageExceptions.UNKNOWN_ERROR));
+        }
+    }
+}
