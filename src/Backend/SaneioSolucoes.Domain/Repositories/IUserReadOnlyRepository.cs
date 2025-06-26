b0303527b4eebfbc139e5eaf75b92d24af7a71b0
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-26 13:03:14 -0300

    feat: add Interfaces to use on Infrastructure

---
+++ src/Backend/SaneioSolucoes.Domain/Repositories/IUserReadOnlyRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SaneioSolucoes.Domain.Repositories
+{
+    public interface IUserReadOnlyRepository
+    {
+        public Task<bool> ExistActiveUserWithEmail(string email);
+        public Task<Entities.User?> GetUserByEmailAndPassword(string email, string password);
+        public Task<bool> ExistActiveUserWithIdentifier(Guid tenantId, Guid userId);
+    }
+}
---
+++ src/Backend/SaneioSolucoes.Domain/Services/LoggedUser/ILoggedUser.cs
@@ -0,0 +1,9 @@
+﻿using SaneioSolucoes.Domain.Entities;
+
+namespace SaneioSolucoes.Domain.Services.LoggedUser
+{
+    public interface ILoggedUser
+    {
+        public Task<User> User();
+    }
+}
