0c03176b41697b3c48c82ff361cd6f493b8ad63e
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-26 18:36:13 -0300

    chore: preparing auto mapper

---
+++ src/Backend/SaneioSolucoes.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using SaneioSolucoes.Communication.Requests;
+using SaneioSolucoes.Communication.Responses;
+
+namespace SaneioSolucoes.Application.Services.AutoMapper
+{
+    public class AutoMapping : Profile
+    {
+
+        public AutoMapping() 
+        {
+            RequestToDomain();
+            DomainToResponse();
+        }
+
+        private void RequestToDomain()
+        {
+        }
+
+        private void DomainToResponse()
+        {
+        }
+    }
+}
