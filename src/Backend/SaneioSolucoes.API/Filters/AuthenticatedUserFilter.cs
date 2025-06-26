0bd020749e34f02bf8fd94e8d694f1700b84f98a
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-26 13:09:16 -0300

    feat: added filter to ensure user existis and is valid with token and on tenant

---
+++ src/Backend/SaneioSolucoes.API/Filters/AuthenticatedUserFilter.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using SaneioSolucoes.Domain.Extensions;
+using SaneioSolucoes.Domain.Repositories;
+using SaneioSolucoes.Domain.Security.Tokens;
+
+namespace SaneioSolucoes.API.Filters
+{
+    public class AuthenticatedUserFilter : IAsyncAuthorizationFilter
+    {
+        private readonly IAccessTokenValidator _accessTokenValidator;
+        private readonly IUserReadOnlyRepository _repository;
+        public AuthenticatedUserFilter(IAccessTokenValidator accessTokenValidator, IUserReadOnlyRepository repository)
+        {
+            _accessTokenValidator = accessTokenValidator;
+            _repository = repository;
+        }
+
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            var token = TokenOnRequest(context);
+
+            var authTokenInfo = _accessTokenValidator.ValidateAndGetUserIdentifier(token);
+
+            var existUser = await _repository.ExistActiveUserWithIdentifier(authTokenInfo.TenantId, authTokenInfo.UserId);
+
+            if (existUser.IsFalse())
+            {
+                //throw new MyRecipeBookException(ResourceMessageExceptions.USER_WITHOUT_PERMISSION_ACCESS_RESOURCE);
+                throw new Exception("USER_WITHOUT_PERMISSION_ACCESS_RESOURCE");
+            }
+        }
+
+        private static string TokenOnRequest(AuthorizationFilterContext context)
+        {
+            var authentication = context.HttpContext!.Request.Headers.Authorization.ToString();
+
+            if (string.IsNullOrWhiteSpace(authentication))
+            {
+                //throw new MyRecipeBookException(ResourceMessageExceptions.NO_TOKEN);
+                throw new Exception("No Token Provided");
+            }
+
+            return authentication["Bearer ".Length..].Trim();
+        }
+    }
+}
