d5c136e48a621b6a9db5519bbd653a6ee844cef2
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-26 18:35:00 -0300

    feat: adding user repository interface and implementation

--- src/Backend/SaneioSolucoes.Domain/Repositories/IUserReadOnlyRepository.cs
+++ src/Backend/SaneioSolucoes.Domain/Repositories/IUserReadOnlyRepository.cs
@@ -2,8 +2,8 @@
 {
     public interface IUserReadOnlyRepository
     {
-        public Task<bool> ExistActiveUserWithEmail(string email);
-        public Task<Entities.User?> GetUserByEmailAndPassword(string email, string password);
+        public Task<bool> ExistActiveUserWithEmail(Guid tenantId, string email);
+        public Task<Entities.User?> GetUserByEmailAndPassword(Guid tenantId, string email, string password);
         public Task<bool> ExistActiveUserWithIdentifier(Guid tenantId, Guid userId);
     }
 }
---
+++ src/Backend/SaneioSolucoes.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaneioSolucoes.Domain.Entities;
+using SaneioSolucoes.Domain.Repositories;
+
+namespace SaneioSolucoes.Infrastructure.DataAccess.Repositories
+{
+    public class UserRepository : IUserReadOnlyRepository
+    {
+        private readonly SaneioSolucoesDBContext _dbContext;
+
+        public UserRepository(SaneioSolucoesDBContext dbContext) => _dbContext = dbContext;
+
+        public Task<bool> ExistActiveUserWithEmail(Guid tenantId, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistActiveUserWithIdentifier(Guid tenantId, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User?> GetUserByEmailAndPassword(Guid tenantId, string email, string password) =>
+            await _dbContext
+                .Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(user => 
+                    user.Active && 
+                    user.TenantId.Equals(tenantId) && 
+                    user.Email.Equals(email) && 
+                    user.Password.Equals(password));
+    }
+}
