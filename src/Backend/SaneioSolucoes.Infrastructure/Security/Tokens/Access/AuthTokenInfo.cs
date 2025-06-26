bba663f53de71f811d5a959bf4a3f96e30e54d9e
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-26 13:06:36 -0300

    feat: adding jwt token generation, hanlder and validator

---
+++ src/Backend/SaneioSolucoes.Infrastructure/Security/Tokens/Access/AuthTokenInfo.cs
@@ -0,0 +1,10 @@
+﻿using SaneioSolucoes.Domain.Security.Tokens;
+
+namespace SaneioSolucoes.Infrastructure.Security.Tokens.Access
+{
+    public class AuthTokenInfo : IAuthTokenInfo
+    {
+        public Guid UserId { get; set; }
+        public Guid TenantId { get; set; }
+    }
+}
---
+++ src/Backend/SaneioSolucoes.Infrastructure/Security/Tokens/Access/Validator/JwtTokenValidator.cs
@@ -0,0 +1,37 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using SaneioSolucoes.Domain.Entities;
+using SaneioSolucoes.Domain.Security.Tokens;
+
+namespace SaneioSolucoes.Infrastructure.Security.Tokens.Access.Validator
+{
+    public class JwtTokenValidator(string signingKey) : JwtTokenHandler, IAccessTokenValidator
+    {
+        private readonly string _signingKey = signingKey;
+
+        public IAuthTokenInfo ValidateAndGetUserIdentifier(string token)
+        {
+            var validationParameter = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                IssuerSigningKey = SecurityKey(_signingKey),
+                ClockSkew = new TimeSpan(0)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var principal = tokenHandler.ValidateToken(token, validationParameter, out _);
+
+            var userIdentifier = principal.Claims.First(c => c.Type == ClaimTypes.Sid).Value;
+            var tenantId = principal.Claims.First(c => c.Type == "tenant_id").Value;
+
+            return new AuthTokenInfo
+            {
+                UserId = Guid.Parse(userIdentifier),
+                TenantId = Guid.Parse(tenantId)
+            };
+        }
+    }
+}
