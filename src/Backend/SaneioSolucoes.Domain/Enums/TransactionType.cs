2e52cc2cc6e23e49fe204de40f7e83edc5a35776
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-26 13:00:30 -0300

    feat: add Enum TransactionType

---
+++ src/Backend/SaneioSolucoes.Domain/Enums/TransactionType.cs
@@ -0,0 +1,24 @@
+﻿namespace SaneioSolucoes.Domain.Enums
+{
+    public enum TransactionType
+    {
+        DEBIT = 0,
+        CREDIT = 1,
+        INT =2,
+        DIV = 3,
+        FEE = 4,
+        SRVCHG = 5,
+        DEP = 6,
+        ATM = 7,
+        POS = 8,
+        XFER = 9,
+        CHECK = 10,
+        PAYMENT = 11,
+        CASH = 12,
+        DIRECTDEP = 13,
+        DIRECTDEBIT = 14,
+        REPEATPMT = 15,
+        HOLD = 16,
+        OTHER = 17
+    }
+}
