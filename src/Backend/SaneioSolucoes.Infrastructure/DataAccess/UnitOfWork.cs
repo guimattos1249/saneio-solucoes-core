e6cd4201e317a0b2e93990db30739efecce485aa
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-26 13:04:16 -0300

    feat: ensure unitOfWork is configured

---
+++ src/Backend/SaneioSolucoes.Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using SaneioSolucoes.Domain.Repositories;
+
+namespace SaneioSolucoes.Infrastructure.DataAccess
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SaneioSolucoesDBContext _dbContext;
+
+        public UnitOfWork(SaneioSolucoesDBContext dbContext) => _dbContext = dbContext;
+
+        public async Task Commit() => await _dbContext.SaveChangesAsync();
+    }
+}
