6895240536bb779ec30b3ce45d5774f8e86a9ce5
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-06-27 12:36:56 -0300

    feat: adding attribute to verify if user is logged

---
+++ src/Backend/SaneioSolucoes.API/Attributes/AuthenticatedUserAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaneioSolucoes.API.Filters;
+
+namespace SaneioSolucoes.API.Attributes
+{
+    public class AuthenticatedUserAttribute : TypeFilterAttribute
+    {
+        public AuthenticatedUserAttribute() : base(typeof(AuthenticatedUserFilter))
+        {
+        }
+    }
+}
